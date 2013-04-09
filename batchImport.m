8497237de87376ff4bc20b678da0e306415df97a
Geoffrey Jones <jonesg5@rpi.edu>
2013-04-09 12:39:32 -0400

    New data

--- batchImport.m
+++ batchImport.m
@@ -6,9 +6,6 @@ directory = uigetdir(startDir,'Select folder with processed Daysimeter files');
 listing = dir(fullfile(directory,'*processed.txt'));
 
 %% Import processed Daysimeter files
-% Initialize experiment start and end dates
-sDate = datenum(2013,3,25);
-eDate = datenum(2013,4,2);
 
 % Preallocate variables
 n = length(listing);
@@ -17,22 +14,34 @@ lux = cell(n,1);
 CLA = cell(n,1);
 CS = cell(n,1);
 activity = cell(n,1);
-subject = cell(n,1);
+subject = zeros(n,1);
+
+% Initialize experiment start and end dates
+sDate = zeros(n,1);
+eDate = zeros(n,1);
+sDate(1) = datenum(2013,3,25);
+eDate(1) = datenum(2013,4,2);
+sDate(2) = datenum(2013,3,25);
+eDate(2) = datenum(2013,4,2);
+sDate(3) = datenum(2013,4,1);
+eDate(3) = datenum(2013,4,9);
+% sDate(4) = datenum(2013,4,1);
+% eDate(4) = datenum(2013,4,9);
 
 for i1 = 1:n
     filename = fullfile(directory,listing(i1).name);
     [time1,lux1,CLA1,CS1,activity1] = importDaysimeter(filename);
+    % Find subject number
+    expression = 'subject(\d*).*';
+    tokens = regexpi(listing(i1).name,expression,'tokens');
+    subject(i1) = str2double(tokens{1});
     % Trim data to experiment period
-    idx = (time1 >= sDate) & (time1 < eDate);
+    idx = (time1 >= sDate(subject(i1))) & (time1 < eDate(subject(i1)));
     time{i1} = time1(idx);
     lux{i1} = lux1(idx);
     CLA{i1} = CLA1(idx);
     CS{i1} = CS1(idx);
     activity{i1} = activity1(idx);
-    % Find subject number
-    expression = 'subject(\d*).*';
-    tokens = regexpi(listing(i1).name,expression,'tokens');
-    subject{i1} = tokens{1};
 end
 
 %% Save imported data
--- firstPass.m
+++ firstPass.m
@@ -8,9 +8,10 @@ close all;
 n = length(time);
 
 for i1 = 1:n
-    Title = ['NIH Sleep Mask Subject: ', cell2mat(subject{i1})];
+    Title = ['NIH Sleep Mask Subject: ', num2str(subject(i1))];
     pseudoActogram(time{i1},activity{i1},CS{i1},Title);
-    fileName = fullfile('pseudoActograms',['subject', cell2mat(subject{i1}),'.png']);
-    print(gcf,'-dpng',fileName,'-r200');
+    fileName = fullfile('pseudoActograms',['subject', num2str(subject(i1))]);
+    print(gcf,'-dpng',[fileName,'.png'],'-r200');
+    print(gcf,'-dpdf',[fileName,'.pdf'],'-r200');
     close;
 end
