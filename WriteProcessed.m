7ccd12dfb73079d692eb505b063bb0c9fe45c0de
Geoffrey Jones <jonesg5@rpi.edu>
2013-05-20 15:26:22 -0400

    Remove Excel support and extraneous data

--- WriteProcessed.m
+++ WriteProcessed.m
@@ -1,21 +1,11 @@
-function WriteProcessed
-%WRITEPROCESSED Write processed data to text and Excel files
-
-% select raw data and header files
-StartPath = '\\ROOT\projects\HANDLS\Files From HANDLS'; % directory to start in
-[InfoName,InfoPath] = uigetfile('*.txt','Select Header/"A" File',StartPath);
-[DataName,DataPath] = uigetfile('*.txt','Select Data File',InfoPath);
-% save the full paths for the source files
-InfoName = fullfile(InfoPath,InfoName);
-DataName = fullfile(DataPath,DataName);
+function WriteProcessed(InfoName,DataName)
+%WRITEPROCESSED Write processed data to text
 
 ProcessedData = ReadRaw(InfoName,DataName);
 time = ProcessedData.time;
-red = ProcessedData.red;
-green = ProcessedData.green;
-blue = ProcessedData.blue;
 lux = ProcessedData.lux;
 CLA = ProcessedData.CLA;
+CS = ProcessedData.CS;
 activity = ProcessedData.activity;
 
 % convert time to string format
@@ -25,23 +15,12 @@ timeStr = datestr(time,'dd/mm/yyyy HH:MM:SS');
 txtName = [DataName(1:end-4) '_Processed.txt']; % name of text file
 fid = fopen(txtName,'w'); % create text file and open for writing
 % write header row to file
-fprintf(fid,'%s\t%s\t%s\t%s\t%s\t%s\t%s\r\n','time','red','green','blue',...
-    'lux','CLA','activity');
+fprintf(fid,'%s\t%s\t%s\t%s\t%s\r\n','time','lux','CLA','CS','activity');
 % write data to file
 for i1 = 1:length(time)
-    fprintf(fid,'%s\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\r\n',...
-    timeStr(i1,:),red(i1),green(i1),blue(i1),lux(i1),CLA(i1),activity(i1));
+    fprintf(fid,'%s\t%.2f\t%.2f\t%.2f\t%.2f\r\n',...
+    timeStr(i1,:),lux(i1),CLA(i1),CS(i1),activity(i1));
 end
 fclose(fid);
-
-% convert MATLAB time to Excel time
-timeXl = m2xdate(time);
-
-% create MS Excel file
-xlName = [DataName(1:end-4) '_Processed.xlsx']; % name of Excel file
-Header = {'time','red','green','blue','lux','CLA','activity'}; % header labels
-xlswrite(xlName,Header,1,'A1'); % write header row to file
-Output = [timeXl',red',green',blue',lux',CLA',activity']; % combine output data
-xlswrite(xlName,Output,1,'A2'); % write data to file
 end
 
